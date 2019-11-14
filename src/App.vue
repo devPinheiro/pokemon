91893caf449342a4e8d2e8a5162b009f0add0fc0
devPinheiro <samuelpinheiro41@gmail.com>
2019-11-14 15:57:57 +0100

    setup landing page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div id="flex m-auto text-center">
-    <div id="bg-gray-45">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="flex ">
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+   <div class="w-full max-w-screen-xl pt-20 pb-32 text-white lg:pt-40 mx-auto px-6 sm:pr-20 sm:pl-20 lg:pr-32 lg:pl-32 block mb-4 bg-blue curve">
+                  <div class="w-full xl:flex lg:flex ">
+                 
+                    <div class="block  xl:flex lg:flex text-center lg:text-left text-dark  ">
+                        <div class="xl:mt-18 xl:pr-32  lg:mt-18 lg:pr-32 ">
+                            <h1 class="font-Montserrat font-bold leading-tight sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl text-xl ">
+                            Pokermon Show
+                            </h1>
+                            <p class="pt-10"> View all the amazing Pokermon</p>
+                        </div>
+                    </div>
+                    <div class="block xl:flex lg:flex pt-8 m-auto">
+                     
+                     <!-- Login form -->
+                     
+                    
+                    </div>
+                  </div>
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+ 
 };
 </script>
+
+
+<style scoped>
+   .curve {
+     border-radius: 0 0 90% 0;
+   }
+   .bg-blue {
+    background: linear-gradient(90deg, rgba(18,35,232,1) 0%, rgb(62, 185, 209) 82%);
+   }
+</style>
