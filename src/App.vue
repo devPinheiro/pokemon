4fd6aa45ff7917795cdc74e5953a3d70d3032ec5
devPinheiro <samuelpinheiro41@gmail.com>
2020-02-16 23:50:48 +0100

    add description

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
                     <div class="block  xl:flex lg:flex text-center lg:text-left text-dark  ">
                         <div class="xl:mt-18 xl:pr-32  lg:mt-18 lg:pr-32 ">
                             <h1 class="font-Montserrat font-bold leading-tight sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl text-xl ">
-                            Pokermon Show
+                            Pokemon Show - A basic vuejs app
                             </h1>
                             <p class="pt-10"> View all the amazing Pokermon</p>
                         </div>
